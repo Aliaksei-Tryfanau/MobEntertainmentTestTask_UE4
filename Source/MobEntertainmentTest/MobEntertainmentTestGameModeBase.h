22e152ae87ebfe5622efbbb2b1b97da44e333749
Aliaksei-Tryfanau <altrishka@gmail.com>
2023-05-10 20:58:29 -0400

    Project creation

---
+++ Source/MobEntertainmentTest/MobEntertainmentTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MobEntertainmentTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBENTERTAINMENTTEST_API AMobEntertainmentTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
